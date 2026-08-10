fa9b449c1e03010bcbff2262deeab75bdc0ed3c0
Severinodeveloper1 <developer1@vesergenperu.com>
2026-08-10 18:43:35 -0500

    update xd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
 
     <title>{{ isset($title) ? $title . ' - ' : '' }}{{ config('app.name', 'IMPORTACIONES DELTA PERU S.A.C') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('img/ISOTIPO%20ORIGINAL.png') }}">
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
